28b7263c716516972f3e17aecbaacfacf888e978
natividadesusana <susanajdsn@gmail.com>
2022-01-19 16:38:26 -0300

    finalizando modal com versiculos aleatórios

--- scripts.js
+++ scripts.js
@@ -2,6 +2,19 @@ const open = document.getElementById('open');
 const modal_container = document.getElementById('modal_container');
 const close = document.getElementById('close');
 
+let quotes = [
+    "Não fui eu que ordenei a você? Seja forte e corajoso! Não se apavore nem desanime, pois o Senhor, o seu Deus, estará com você por onde você andar. Josué 1:9",
+    "Eu disse essas coisas para que em mim vocês tenham paz. Neste mundo vocês terão aflições; contudo, tenham ânimo! Eu venci o mundo. João 16:33",
+    "Por isso não tema, pois estou com você; não tenha medo, pois sou o seu Deus. Eu o fortalecerei e o ajudarei; eu o segurarei com a minha mão direita vitoriosa. Isaías 41:10",
+    "Porque sou eu que conheço os planos que tenho para vocês', diz o Senhor, 'planos de fazê-los prosperar e não de causar dano, planos de dar a vocês esperança e um futuro. Jeremias 29:11",
+    "Deem graças em todas as circunstâncias, pois esta é a vontade de Deus para vocês em Cristo Jesus. 1 Tessalonicenses 5:18",
+]
+
+function getQuote(){
+    let randomVerse = Math.floor(Math.random() * quotes.length);
+    document.getElementById('newQuoteSection').innerHTML = quotes[randomVerse];
+} 
+
 open.addEventListener('click', () => {
     modal_container.classList.add('show')
 });
